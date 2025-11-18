36fda881b5a54997db40f7e09debb0370e62a7e2
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-11-17 22:02:52 -0600

    Done With All Crud Operations

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -8,5 +8,65 @@ export const getUsers = async (req, res) => {
     }
     catch(error){
         console.log("Failed Fetching Users", error);
+        res.status(500).json({message:"Failed to Fetch Data"});
+    }
+}
+
+export const addUser = async (req, res) => {
+    try{
+        const {name, email, password} = req.body;
+        const user = new User({name, email, password});
+        await user.save();
+        return res.status(201).json({
+            success:true,
+            message:"User Added Succesfully",
+            user
+        })
+    }
+    catch(error){
+        console.log("Error in Adding User Data", error);
+        res.status(500).json({message:"Failed to Add User"});
+    }
+}
+
+export const updateUser = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const updateData = req.body;
+
+        const updatedUser = await User.findByIdAndUpdate(id, updateData,
+            {new:true}
+        )
+
+        if(!updatedUser){
+            return res.status(404).json({message:"User Not Found"});
+        }
+
+
+        return res.status(200).json({
+            message:"User Updated Succesfully",
+            updatedUser
+        })
+    }
+    catch(error){
+        console.error("Error in Updating Results", error);
+        res.status(500).json({message:"Error in Updating!"});
+    }
+}
+
+export const deleteUser = async(req, res) => {
+    try{
+        const {id} = req.params;
+        const deleteUser = User.findByIdAndDelete(id,
+            {new:true}
+        )
+        return res.status(201).json({
+            message:"User Deleted Succesfully",
+            user: deleteUser
+        })
+    }
+    catch(error){
+        console.error("Error", error);
+        res.status(500).json({message:"User Deleted Succesfully"});
     }
 }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,11 @@
-import {getUsers} from "../controllers/userController.js";
+import {getUsers, addUser, updateUser, deleteUser} from "../controllers/userController.js";
 import express from "express";
 
 const router = express.Router();
 
 router.get("/", getUsers);
+router.post("/", addUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
 export default router;
