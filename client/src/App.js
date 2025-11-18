9ae8907dce25d2ca33677b99056b99da0e841d3e
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-11-17 19:13:57 -0600

    Frontend Setup

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Welcome to the JWT</h1>
+    </div>
+  );
+}
+
+export default App;
